6845c41d7e5b6794dabdcbf2c0df38e4c245aec9
Raul Santamaria <raulsantamariao@hotmail.com>
2026-05-07 21:09:08 -0700

    feat(devcontainer): update Dockerfile and devcontainer.json for user permissions and path adjustments

--- .devcontainer/script/post-create.sh
+++ .devcontainer/script/post-create.sh
@@ -2,6 +2,9 @@
 set -eu
 
 git config --global --add safe.directory /workspace
+git config --global --add safe.directory /opt/flutter
+
+dart pub global activate protoc_plugin
 
 flutter config --no-enable-linux-desktop
 
